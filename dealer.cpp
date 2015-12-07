62cdcc375a25537f41ef194aa210cb0802d44fa2
chasemartin <chasekmartin@gmail.com>
2015-12-06 22:37:18 -0600

    Update dealer.cpp

--- dealer.cpp
+++ dealer.cpp
@@ -9,7 +9,7 @@ bool Dealer::hit_or_stay() //dealer hits on soft 17
 	if (num_aces==0)
 	{
 		return non_ace_total<17 ? true:false; //dealer stands on hard 17 and above
-	}
+	};
 	else 
 	{
 		score_low=non_ace_total + num_aces;
@@ -18,14 +18,14 @@ bool Dealer::hit_or_stay() //dealer hits on soft 17
 		if (score_high<18) //hit on soft 17 or lower
 		{
 			return true;
-		}
+		};
 		else if (score_high>21 && score_low<17) //lower score is below 17, aces are considered as 1
 		{
 			return true;
-		}
+		};
 		else //higher score is more than 17 or lower score is more than 16
 		{
 			return false;
-		}
-	}
-}
+		};
+	};
+};
