19f95cf7706e72f0212270d93d598ae2ca8bb156
chasemartin <chasekmartin@gmail.com>
2015-12-06 22:39:31 -0600

    Update gtest.cpp

--- gtest.cpp
+++ gtest.cpp
@@ -9,23 +9,23 @@
 TEST(Deck, NewDeckHas52Cards){
   thrust::host_vector<int> deck(52);
   EXPECT_EQ(52, deck.size());
-}
+};
 
 TEST(Deck, Shuffle){
   thrust::host_vector<int> deck(52);
   shuffleDeck();
   EXPECT_FALSE(shuffle(52)=deck(52));
-}
+};
 
 TEST(Player, Win){
   int p.bestscore = 13;
   int computer.bestscore = 21;
     ASSERT_TRUE(playerWon(p.bestscore>computer.bestscore));
     ASSERT_FALSE(playerWon(p.bestscore<computer.bestscore));
-}
+};
 
 int main( int argc, char ** argv){
     ::testing::InitGoogleTest(&argc, argv);
       return RUN_ALL_TESTS();
-}
-}
+};
+};
