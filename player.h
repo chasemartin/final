91f341d81b115a3e8eadb361598bd4fc8fb9cbd2
rachaelvphillips <rachaelvphillips@gmail.com>
2015-12-07 09:44:26 -0600

    Update player.h

--- player.h
+++ player.h
@@ -10,8 +10,10 @@
 //included dependencies
 
 #include <vector>
+#include <thrust/host_vector.h>
 #include <string>
 #include "deck.h"
+#include "card.h"
 #include "participant.h"
 
 //=======================
