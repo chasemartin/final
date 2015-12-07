3a88e00b2e24e7a594aa01e131547df13f0489ec
chasemartin <chasekmartin@gmail.com>
2015-12-06 22:45:45 -0600

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -10,7 +10,7 @@ Player::Player()	//initialize class variables
 	if(num_aces==0)
 	{
 		return non_ace_total<15 ? true:false;
-	}
+	};
 	else
         {
                 score_low=non_ace_total + num_aces;
@@ -18,32 +18,32 @@ Player::Player()	//initialize class variables
                 if(score_high<16)                       //higher score is 15 or less (hit on soft 15 or lower)
                 {
                         return true;
-                }
+                };
                 else if (score_high>21 &&score_low <15) //lower score is below 15 and all aces considered 1
                 {
                         return true;
-                }
+                };
                 else                                    //higher score is more than 15 or lower score is more than 14
                 {
                         return false;
-                }
-        }
-}
+                };
+        };
+};
 
 double Player::getWinpercentage()
 {
 	return (double)gamesWon/gamesPlayer;	//returns win percentage
-}
+};
 int Player::numPlayed()
 {
 	return gamesPlayed;
-}
+};
 int Player::numWon()
 {
 	return gamesWon;
-}
+};
 int Player::numTied()
 {
 	return gamesTied;
-}
+};
 
