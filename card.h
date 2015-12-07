293ea5748a0d8d8e7883b4aa78935f264a1eee69
rachaelvphillips <rachaelvphillips@gmail.com>
2015-12-07 09:34:50 -0600

    Update card.h

--- card.h
+++ card.h
@@ -4,12 +4,12 @@
 #include "deck.h"
 #include <thrust/host_vector.h>
 
-class Card
+class Card : public Deck //card inherits from Deck
 {
   thrust::host_vector<int> deck(52);
   int value;
   public:
-    Card(int c);
-    int getValue();
+    Card(int c);    //c is the value of the card from the deck
+    int getValue(); //returns the value
 };
 #endif
